2aeee716183d55ee8b84ad54fe8f7b52664079f0
nao11aihara <nao11aihara@gmail.com>
2022-02-07 00:02:37 +0900

    add: ショップモデルの実装方針を追加

--- app/models/shops.go
+++ app/models/shops.go
@@ -1,1 +1,21 @@
 package models
+
+// モデル
+// 構造体定義、DBにアクセスする関数、レシーバメソッドを記載する。
+
+// import (
+// 	"fmt"
+// )
+
+// type Sample struct {
+// 	Test1 string `json:"test1"`
+// 	Test2 int `json:"test2"`
+// }
+
+// func (s *Sample) SampleReceiver() {
+// 	fmt.Println(s.Test1, s.Test2)
+// }
+
+// func SampleFunction(id string) {
+// 	fmt.Println(id)
+// }
